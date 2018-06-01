1592066aa399adfcc43c07c231ce6cc3ee1984f4
Piyush Patel <piyushpp1119@gmail.com>
2018-06-01 15:37:11 +0530

    This has the initial code in music/views.py which is to make list all the albums stored in the database . But this makes things clutter so we will be doing something else to make it look better

--- music/urls.py
+++ music/urls.py
@@ -3,5 +3,10 @@ from . import views
 #from django.contrib import admin
 
 urlpatterns = [
+        # for music home
     url(r'^$', views.index , name='index'),
+
+        #for music home/AlbumId/
+        #[0-9] tells that the id we will be receiving is a number and the + after indicates the this would be a long integer
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail , name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,4 +1,16 @@
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1>This is the first code</h1>")
+    #a variable that gets all the albums by connection to database
+    all_album=Album.objects.all()
+    html=''
+
+    for album in all_album:
+        url=',/music/'+str(album.id)+'/'
+        html+='<a href="'+url+'"">'+album.album_title+'<a><br>'
+
+    return HttpResponse(html)
+
+def detail(request,album_id):
+    return HttpResponse("<h2>Details for Album Id :"+str(album_id)+"</h2>")
